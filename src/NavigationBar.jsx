b3396ce5b72231f760e4025ddae31cbbcde05eec
Ziad-J05 <143660614+Ziad-J05@users.noreply.github.com>
2025-07-25 12:00:13 -0400

    Installed Flowbite React and added navbar

--- src/NavigationBar.jsx
+++ src/NavigationBar.jsx
@@ -1,34 +1,24 @@
 import './index.css'
+import { Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
 
 function NavigationBar(){
   return (
     <>
-    {/* Nav Bar */}
-      <header className="fixed top-[0px] left-[0px] w-full">
-        <nav className="px-[1em] py-[0.5em] mx-auto w-2xl mt-[1rem] bg-white rounded-xs shadow-md">
-          <ul className="flex justify-evenly gap-x-[1em]">
-            <li>
-              <a className="text-xl hover:underline" href="#skills">
-                Skills
-              </a>
-            </li>
-            <li>
-              <a className="text-xl hover:underline" href="#experience">
-                Experience
-              </a>
-            </li>
-            <li>
-              <a className="text-xl hover:underline" href="#projects">
-                Projects
-              </a>
-            </li>
-            <li>
-              <a className="text-xl hover:underline" href="#contact">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </nav>
+      <header className="fixed w-full">
+      <Navbar fluid rounded>
+        <NavbarBrand href="https://flowbite-react.com">
+          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Ziad Jaafar</span>
+        </NavbarBrand>
+        <NavbarToggle />
+        <NavbarCollapse>
+          <NavbarLink href="#skills">
+            Skills
+          </NavbarLink>
+          <NavbarLink href="#expereince">Experience</NavbarLink>
+          <NavbarLink href="#projects">Projects</NavbarLink>
+          <NavbarLink href="#contact">Contact</NavbarLink>
+        </NavbarCollapse>
+      </Navbar>
       </header>
     </>
   );
