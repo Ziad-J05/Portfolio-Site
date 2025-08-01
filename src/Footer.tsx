d3cc62d1de975c855d56c46b3ce4db6c5738fcb2
Ziad-J05 <143660614+Ziad-J05@users.noreply.github.com>
2025-08-01 16:43:20 -0400

    changed footer layout and added content

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -4,25 +4,33 @@ function Footer(){
   return(
     <>
       {/* Contact */}
-      <footer
-        id="contact"
-        className="mx-auto w-4xl pt-[1rem] pb-[3rem] bg-slate-700"
-      >
-        <h2 className="text-5xl w-2xl mx-auto mb-[1rem] text-white">Contact</h2>
-        <ul className="w-2xl mx-auto">
+      <footer className="mx-auto w-4xl pt-[1rem] pb-[3rem] bg-slate-700">
+        <ul className="w-2xl mx-auto flex gap-4 justify-center">
           <li>
-            <p className="text-white">Email</p>
+            <a href="/" className="text-white hover:underline">Home</a>
           </li>
           <li>
-            <p className="text-white">Phone</p>
+            <a href="/projects" className="text-white hover:underline">Projects</a>
           </li>
           <li>
-            <a className="text-white hover:underline">LinkedIn</a>
-          </li>
-          <li>
-            <a className="text-white hover:underline">GitHub</a>
+            <a href="/contact" className="text-white hover:underline">Contact</a>
           </li>
         </ul>
+
+        <ul className="w-2xl mx-auto flex gap-4 justify-center">
+          <li>
+            <a href="" className="text-white hover:underline">Resume</a>
+          </li>
+          <li>
+            <a href="" className="text-white hover:underline">LinkedIn</a>
+          </li>
+          <li>
+            <a href="" className="text-white hover:underline">GitHub</a>
+          </li>
+        </ul>
+        
+        <hr className='w-2xl text-white mx-auto my-4'></hr>
+        <p className='w-2xl mx-auto text-white text-center'>&copy;2025 Ziad Jaafar - Made with React and Tailwind</p>
       </footer>
     </>
   )
