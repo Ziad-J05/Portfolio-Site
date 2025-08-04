045919c18a9c711ab7e4c3cbaf1722e5ff67ef74
Ziad-J05 <143660614+Ziad-J05@users.noreply.github.com>
2025-08-04 13:17:49 -0400

    added new main component to all pages

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,5 +1,6 @@
 import '../index.css'
 import NavigationBar from '../components/NavigationBar'
+import MainContentArea from '../components/MainContentArea'
 import ProjectArea from '../components/ProjectArea'
 import { ProjectInfo } from '../components/Project'
 import Footer from '../components/Footer'
@@ -19,12 +20,13 @@ export default function ProjectsPage() {
   return (
     <>
         <NavigationBar />
-        <main className="bg-[#ffffff] py-36 mx-auto w-4xl">
-            <div className="w-2xl mx-auto">
+        <MainContentArea content={[
+          <div className="w-2xl mx-auto">
                 <h2 className="text-5xl mb-[1rem]">All Projects</h2>
                 <ProjectArea projectInfoList={projectInfo} />
-            </div>
-        </main>
+          </div>
+        ]} />
+            
         <Footer />
     </>
   )
