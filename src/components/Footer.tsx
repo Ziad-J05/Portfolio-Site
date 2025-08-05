f3addef431a3de710e7dcc21c172243b9c8679a0
Ziad-J05 <143660614+Ziad-J05@users.noreply.github.com>
2025-08-05 16:36:39 -0400

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,34 +3,8 @@ import '../index.css'
 function Footer(){
   return(
     <>
-      {/* Contact */}
-      <footer className="mx-auto w-4xl pt-[1rem] pb-[3rem] bg-slate-700">
-        <ul className="w-2xl mx-auto flex gap-4 justify-center">
-          <li>
-            <a href="/" className="text-white hover:underline">Home</a>
-          </li>
-          <li>
-            <a href="/projects" className="text-white hover:underline">Projects</a>
-          </li>
-          <li>
-            <a href="/contact" className="text-white hover:underline">Contact</a>
-          </li>
-        </ul>
-
-        <ul className="w-2xl mx-auto flex gap-4 justify-center">
-          <li>
-            <a href="" className="text-white hover:underline">Resume</a>
-          </li>
-          <li>
-            <a href="" className="text-white hover:underline">LinkedIn</a>
-          </li>
-          <li>
-            <a href="" className="text-white hover:underline">GitHub</a>
-          </li>
-        </ul>
-        
-        <hr className='w-2xl text-white mx-auto my-4'></hr>
-        <p className='w-2xl mx-auto text-white text-center'>&copy;2025 Ziad Jaafar - Made with React and Tailwind</p>
+      <footer className="mx-auto text-lg w-4xl py-4 bg-white text-black border-t-2">
+        <p className='w-2xl mx-auto text-center'>&copy;2025 Ziad Jaafar - Made with React and Tailwind</p>
       </footer>
     </>
   )
