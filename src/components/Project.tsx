9af7d6b6735f76aad9795a8c90fa26ab7da6d370
Ziad-J05 <143660614+Ziad-J05@users.noreply.github.com>
2025-08-05 11:46:30 -0400

    began styling project cards

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -35,8 +35,8 @@ export class FeaturedProjectInfo extends ProjectInfo {
 const cardTheme = createTheme({
   card: {
     root: {
-      "base": "flex rounded-lg border border-gray-200 bg-white shadow-md dark:border-gray-700 dark:bg-gray-800",
-      "children": "flex h-full flex-col justify-center gap-4 p-6",
+      "base": "flex rounded-lg border-2 border-black shadow-none",
+      "children": "flex h-full w-full flex-col justify-center gap-4 p-0",
       horizontal: {
         "off": "flex-col",
         "on": "flex-col md:max-w-2xl md:flex-row"
@@ -46,8 +46,8 @@ const cardTheme = createTheme({
     img: {
       "base": "",
       horizontal: {
-        off: "rounded-t-lg h-64 object-cover",
-        on: "h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+        off: "rounded-t-md h-64 object-cover border-b-2",
+        on: "h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-md border-r-2"
       }
     }
   }
@@ -63,23 +63,26 @@ export function FeaturedProject( {projectInfo}: {projectInfo: FeaturedProjectInf
                 imgSrc={projectInfo.img}
             >
                 <div>
-                <div className="flex items-baseline gap-2">
-                    <h3 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
+                <div className="flex items-baseline border-b-2">
+                    <h3 className="text-3xl font-bold tracking-tight px-2 py-1 text-black">
                         {projectInfo.name}
                     </h3>
-                    <p className="text-2xl text-gray-700 dark:text-gray-400 mb-0">
+                    <div className="border-l-2 self-stretch"></div>
+                    <p className="text-3xl text-black px-2 py-1 mb-0">
                         {projectInfo.role}
                     </p>
                 </div>
 
-                <div className="flex gap-2 my-0.5">
-                    <SkillsArea skills={projectInfo.skills} />
-                </div>
-                
-                <p className="font-normal text-gray-700 dark:text-gray-400">
-                    {projectInfo.body}
-                </p>
-                <a className="hover:underline" href={projectInfo.link}>More info</a>
+                <div className="px-2 py-1">
+                    <div className="flex gap-2 my-0.5">
+                        <SkillsArea skills={projectInfo.skills} />
+                    </div>
+                    
+                    <p className="font-normal text-gray-700 dark:text-gray-400">
+                        {projectInfo.body}
+                    </p>
+                    <a className="hover:underline" href={projectInfo.link}>More info</a>
+                    </div>
                 </div>
             </Card>
         </ThemeProvider>
@@ -100,17 +103,20 @@ export function Project( {projectInfo}: {projectInfo: ProjectInfo} ){
             horizontal
             >
                 <div>
-                <h3 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                    {projectInfo.name}
-                </h3>
-                <p className="text-xl text-gray-700 dark:text-gray-400 mb-0">
-                    {projectInfo.role}
-                </p>
-                <div className="flex gap-2 my-0.5">
-                    <SkillsArea skills={projectInfo.skills} />
+                    <h3 className="text-2xl font-bold tracking-tight text-black border-b-2 w-full px-2 py-1">
+                        {projectInfo.name}
+                    </h3>
+                    <div className="px-2 py-1">
+                        <p className="text-2xl text-black mb-0">
+                            {projectInfo.role}
+                        </p>
+                        <div className="flex gap-2 my-0.5">
+                            <SkillsArea skills={projectInfo.skills} />
+                        </div>
+                        <a className="hover:underline" href={projectInfo.link}>More info</a>
+                    </div>
                 </div>
-                <a className="hover:underline" href={projectInfo.link}>More info</a>
-                </div>
+
             </Card>
         </ThemeProvider>
         </>
