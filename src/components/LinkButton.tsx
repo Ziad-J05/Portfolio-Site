28df73919dbd8c623705ba9c75c02085ad16b558
Ziad-J05 <143660614+Ziad-J05@users.noreply.github.com>
2025-08-05 11:05:06 -0400

    finished styling link button

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -13,10 +13,9 @@ interface LinkButtonProps {
 
 const buttonTheme = createTheme({
     button: {
-        base: "relative flex shadow-[4px_6px_0px_-1px_rgba(0,0,0,1)] items-center justify-center rounded-lg text-center font-medium",
+        base: "relative flex shadow-[4px_6px_0px_-1px_rgba(0,0,0,1)] w-fit items-center justify-center rounded-lg text-center font-medium hover:-top-[2px] hover:-left-[2px] hover:shadow-[6px_8px_0px_-1px_rgba(0,0,0,1)] active:top-[2px] active:left-[2px] active:shadow-[2px_4px_0px_-1px_rgba(0,0,0,1)]",
         disabled: "pointer-events-none opacity-50",
         fullSized: "w-full",
-        grouped: "rounded-none border-l-0 first:rounded-s-lg first:border-l last:rounded-e-lg focus:ring-2",
         pill: "rounded-full",
         size: {
             xs: "h-8 px-3 text-xs",
@@ -26,18 +25,15 @@ const buttonTheme = createTheme({
             xl: "h-[52px] px-6 text-base"
         },
         color: {
-            default: "bg-white text-black hover:bg-primary-800 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800",
+            default: "bg-white hover:bg-white text-black border-2",
         },
-        outlineColor: {
-            default: "border-8 border-black text-primary-700",
-        }
     }   
 })
 
 export default function LinkButton({text, link}: LinkButtonProps){
     return(
         <ThemeProvider theme={buttonTheme}>
-            <Button as="a" href={link} color="default" className='w-fit border'>{text}</Button>
+            <Button as="a" href={link} color="default" className=''>{text}</Button>
         </ThemeProvider>
     );
 }
