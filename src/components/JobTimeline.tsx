f7a5c91072ef2f1797495a336934c2ceff8016fd
Ziad-J05 <143660614+Ziad-J05@users.noreply.github.com>
2025-08-05 15:17:06 -0400

    began implementing timeline style

--- src/components/JobTimeline.tsx
+++ src/components/JobTimeline.tsx
@@ -33,25 +33,45 @@ const timelineTheme = createTheme({
     root: {
     direction: {
       horizontal: "sm:flex",
-      vertical: "relative border-l border-gray-700 dark:border-gray-700"
+      vertical: "relative border-l-2 border-black dark:border-gray-700"
       }
     },
     item: {
-    point: {
-      horizontal: "flex items-center",
-      line: "hidden h-0.5 w-full bg-blue-600 sm:flex dark:bg-gray-700",
-      marker: {
-        base: {
-          horizontal: "absolute -left-1.5 h-3 w-3 rounded-full border border-amber-400 bg-amber-400 dark:border-gray-900 dark:bg-gray-700",
-          vertical: "absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-700 dark:border-gray-900 dark:bg-gray-700"
+      root: {
+      horizontal: "relative mb-6 sm:mb-0",
+      vertical: "mb-10 ml-6"
+      },
+      content: {
+        root: {
+          base: "border-2 border-black rounded-lg py-1 px-2",
+          horizontal: "mt-3 sm:pr-8",
+          vertical: ""
         },
-        icon: {
-          base: "h-3 w-3 text-primary-600 dark:text-primary-300",
-          wrapper: "absolute -left-3 flex h-6 w-6 items-center justify-center rounded-full bg-primary-200 ring-8 ring-white dark:bg-primary-900 dark:ring-gray-900"
+        body: {
+          base: "mb-1 text-lg font-normal text-black dark:text-gray-400"
+        },
+        time: {
+          base: "mb-1 text-lg font-normal leading-none text-black dark:text-gray-500"
+        },
+        title: {
+          base: "text-2xl font-semibold text-black dark:text-white"
         }
       },
-      vertical: ""
-    }
+      point: {
+        horizontal: "flex items-center",
+        line: "hidden h-0.5 w-full bg-blue-600 sm:flex dark:bg-gray-700",
+        marker: {
+          base: {
+            horizontal: "absolute -left-1.5 h-3 w-3 rounded-full border border-amber-400 bg-amber-400 dark:border-gray-900 dark:bg-gray-700",
+            vertical: "absolute -left-2 mt-0 h-4 w-4 rounded-full border-2 border-black bg-white dark:border-gray-900 dark:bg-gray-700"
+          },
+          icon: {
+            base: "h-3 w-3 text-primary-600 dark:text-primary-300",
+            wrapper: "absolute -left-3 flex h-6 w-6 items-center justify-center rounded-full bg-primary-200 ring-8 ring-white dark:bg-primary-900 dark:ring-gray-900"
+          }
+        },
+        vertical: ""
+      }
   }
   }
 });
