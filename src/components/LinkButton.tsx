fdfffe458cd9b9f4eb28b7cf38187197ae583050
Ziad-J05 <143660614+Ziad-J05@users.noreply.github.com>
2025-08-06 16:15:39 -0400

    fixed link button effects

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -13,7 +13,7 @@ interface LinkButtonProps {
 
 const buttonTheme = createTheme({
     button: {
-        base: "relative flex shadow-[2px_4px_0px_-1px_rgba(0,0,0,1)] w-fit items-center justify-center rounded-lg text-center font-medium hover:-translate-[2px] hover:shadow-[4px_6px_0px_-1px_rgba(0,0,0,1)] active:top-[2px] active:left-[2px] active:shadow-[0px_2px_0px_-1px_rgba(0,0,0,1)]",
+        base: "relative flex shadow-[2px_4px_0px_-1px_rgba(0,0,0,1)] w-fit items-center justify-center rounded-lg text-center font-medium hover:-translate-[2px] hover:shadow-[4px_6px_0px_-1px_rgba(0,0,0,1)] active:translate-[2px] active:shadow-[0px_2px_0px_-1px_rgba(0,0,0,1)] focus:ring-0",
         disabled: "pointer-events-none opacity-50",
         fullSized: "w-full",
         pill: "rounded-full",
