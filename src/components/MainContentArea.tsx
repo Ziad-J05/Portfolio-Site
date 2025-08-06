66986bfe51f8b962b282cb212b69a342ad3f6641
Ziad-J05 <143660614+Ziad-J05@users.noreply.github.com>
2025-08-06 15:58:28 -0400

    fixed contact page

--- src/components/MainContentArea.tsx
+++ src/components/MainContentArea.tsx
@@ -5,7 +5,7 @@ function MainContentArea({content}: {content: ReactElement[]}){
 
   return(
     <>
-      <main className="bg-[#F7EDE2] py-36 mx-auto w-4xl border-l-2 border-r-2">
+      <main className="bg-[#F7EDE2] py-36 mx-auto w-4xl min-h-[calc(100vh-var(--text-lg)-8*var(--spacing))] border-l-2 border-r-2">
           {content}
       </main>
     </>
