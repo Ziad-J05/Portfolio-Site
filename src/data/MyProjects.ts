3b999c0efe832947582a60c5b90bae0cf53c9ff0
Ziad-J05 <143660614+Ziad-J05@users.noreply.github.com>
2025-08-11 17:23:09 -0400

    restore project content

--- src/data/MyProjects.ts
+++ src/data/MyProjects.ts
@@ -3,9 +3,49 @@ import { MySkills } from "./MySkills"
 
 export const MyProjects: ProjectData[] = [
     
-    {   projectName: 'Project One',
-        companyName: 'Sample company name' ,
-        role: 'Sample role',
+    {   projectName: 'Eulyss',
+        companyName: 'Personal Project' ,
+        role: 'Programmer',
+        skills: [MySkills.Unity, MySkills.CSharp],
+        thumbnailSrc: '/placeholder-photo.jpg',
+        description: [
+            'Collaborated as part of a team of two to create an adventure game with a custom text parser that imports text files as graphs for in-game dialogue. Currently in development. ',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non lacus molestie, rhoncus ipsum ut, convallis mauris. Pellentesque sed dignissim sem, eget commodo ante. Donec ligula mi, sollicitudin non nulla a, porttitor ultrices orci. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent finibus sed ligula a ornare. Vestibulum vehicula accumsan ante, id feugiat est fringilla at. Phasellus ullamcorper sodales augue, et malesuada mi. ',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non lacus molestie, rhoncus ipsum ut, convallis mauris. Pellentesque sed dignissim sem, eget commodo ante. Donec ligula mi, sollicitudin non nulla a, porttitor ultrices orci. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent finibus sed ligula a ornare. Vestibulum vehicula accumsan ante, id feugiat est fringilla at. Phasellus ullamcorper sodales augue, et malesuada mi. ',
+        ],
+        sampleLink:'https://cytastorm.itch.io/eulyss-prologue' ,
+        githubLink:'https://github.com/CytaStorm/Taken', 
+        processImages:[
+            { source:"/placeholder-photo.jpg", caption:"This is a description. Hello!"},
+            { source:"/placeholder-photo.jpg", caption:"This is another description."},
+            { source:"/placeholder-photo.jpg", caption:""},
+            { source:"/placeholder-photo.jpg", caption:"That last one had no description."},
+        ]
+    },
+
+    {   projectName: 'Pokedex Search Website',
+        companyName: 'School Project' ,
+        role: 'Solo Developer',
+        skills: [MySkills.HTML, MySkills.CSS, MySkills.JavaScript],
+        thumbnailSrc: '/placeholder-photo.jpg',
+        description: [
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non lacus molestie, rhoncus ipsum ut, convallis mauris. Pellentesque sed dignissim sem, eget commodo ante. Donec ligula mi, sollicitudin non nulla a, porttitor ultrices orci. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent finibus sed ligula a ornare. Vestibulum vehicula accumsan ante, id feugiat est fringilla at. Phasellus ullamcorper sodales augue, et malesuada mi. ',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non lacus molestie, rhoncus ipsum ut, convallis mauris. Pellentesque sed dignissim sem, eget commodo ante. Donec ligula mi, sollicitudin non nulla a, porttitor ultrices orci. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent finibus sed ligula a ornare. Vestibulum vehicula accumsan ante, id feugiat est fringilla at. Phasellus ullamcorper sodales augue, et malesuada mi. ',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non lacus molestie, rhoncus ipsum ut, convallis mauris. Pellentesque sed dignissim sem, eget commodo ante. Donec ligula mi, sollicitudin non nulla a, porttitor ultrices orci. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent finibus sed ligula a ornare. Vestibulum vehicula accumsan ante, id feugiat est fringilla at. Phasellus ullamcorper sodales augue, et malesuada mi. ',
+        ],
+        sampleLink:'#' ,
+        githubLink:'#', 
+        processImages:[
+            { source:"/placeholder-photo.jpg", caption:"This is a description. Hello!"},
+            { source:"/placeholder-photo.jpg", caption:"This is another description."},
+            { source:"/placeholder-photo.jpg", caption:""},
+            { source:"/placeholder-photo.jpg", caption:"That last one had no description."},
+        ]
+    },
+
+    {   projectName: 'Impetus',
+        companyName: 'Personal Project' ,
+        role: 'Programmer, Lead Artist',
         skills: [MySkills.Unity, MySkills.CSharp],
         thumbnailSrc: '/placeholder-photo.jpg',
         description: [
@@ -23,10 +63,10 @@ export const MyProjects: ProjectData[] = [
         ]
     },
 
-    {   projectName: 'Project Two',
-        companyName: 'Sample company name' ,
-        role: 'Sample role',
-        skills: [MySkills.Unity, MySkills.CSharp],
+    {   projectName: 'Space Trucker',
+        companyName: 'School Project' ,
+        role: 'Solo Developer',
+        skills: [MySkills.HTML, MySkills.CSS, MySkills.JavaScript, MySkills.PixiJS],
         thumbnailSrc: '/placeholder-photo.jpg',
         description: [
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non lacus molestie, rhoncus ipsum ut, convallis mauris. Pellentesque sed dignissim sem, eget commodo ante. Donec ligula mi, sollicitudin non nulla a, porttitor ultrices orci. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent finibus sed ligula a ornare. Vestibulum vehicula accumsan ante, id feugiat est fringilla at. Phasellus ullamcorper sodales augue, et malesuada mi. ',
@@ -43,10 +83,30 @@ export const MyProjects: ProjectData[] = [
         ]
     },
 
-    {   projectName: 'Project Three',
+    {   projectName: 'Gravity Game',
+        companyName: 'School Project' ,
+        role: 'Solo Developer',
+        skills: [MySkills.CPlusPlus, MySkills.SFML, MySkills.Box2D],
+        thumbnailSrc: '/placeholder-photo.jpg',
+        description: [
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non lacus molestie, rhoncus ipsum ut, convallis mauris. Pellentesque sed dignissim sem, eget commodo ante. Donec ligula mi, sollicitudin non nulla a, porttitor ultrices orci. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent finibus sed ligula a ornare. Vestibulum vehicula accumsan ante, id feugiat est fringilla at. Phasellus ullamcorper sodales augue, et malesuada mi. ',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non lacus molestie, rhoncus ipsum ut, convallis mauris. Pellentesque sed dignissim sem, eget commodo ante. Donec ligula mi, sollicitudin non nulla a, porttitor ultrices orci. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent finibus sed ligula a ornare. Vestibulum vehicula accumsan ante, id feugiat est fringilla at. Phasellus ullamcorper sodales augue, et malesuada mi. ',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non lacus molestie, rhoncus ipsum ut, convallis mauris. Pellentesque sed dignissim sem, eget commodo ante. Donec ligula mi, sollicitudin non nulla a, porttitor ultrices orci. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent finibus sed ligula a ornare. Vestibulum vehicula accumsan ante, id feugiat est fringilla at. Phasellus ullamcorper sodales augue, et malesuada mi. ',
+        ],
+        sampleLink:'#' ,
+        githubLink:'#', 
+        processImages:[
+            { source:"/placeholder-photo.jpg", caption:"This is a description. Hello!"},
+            { source:"/placeholder-photo.jpg", caption:"This is another description."},
+            { source:"/placeholder-photo.jpg", caption:""},
+            { source:"/placeholder-photo.jpg", caption:"That last one had no description."},
+        ]
+    },
+
+    {   projectName: 'Johnny Rocket',
         companyName: 'Sample company name' ,
         role: 'Sample role',
-        skills: [MySkills.Unity, MySkills.CSharp],
+        skills: [MySkills.CSharp, MySkills.Monogame],
         thumbnailSrc: '/placeholder-photo.jpg',
         description: [
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non lacus molestie, rhoncus ipsum ut, convallis mauris. Pellentesque sed dignissim sem, eget commodo ante. Donec ligula mi, sollicitudin non nulla a, porttitor ultrices orci. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent finibus sed ligula a ornare. Vestibulum vehicula accumsan ante, id feugiat est fringilla at. Phasellus ullamcorper sodales augue, et malesuada mi. ',
--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -9,29 +9,6 @@ import Footer from '../components/Footer'
 import { MySkills} from '../data/MySkills'
 import { MyProjects } from '../data/MyProjects'
 
-/*
-const projectInfo:ProjectInfo[] = 
-[
-  new ProjectInfo("Eulyss", "Programmer", [MySkills.Unity, MySkills.CSharp], "/projects/project-info", "/placeholder-photo.jpg"),
-  new ProjectInfo("Pokedex Search Website", "Solo Developer", [MySkills.HTML, MySkills.CSS, MySkills.JavaScript], "/projects/project-info", "/placeholder-photo.jpg"),
-  new ProjectInfo("Impetus", "Programmer", [MySkills.Unity, MySkills.CSharp], "/projects/project-info", "/placeholder-photo.jpg"),
-  new ProjectInfo("Space Trucker", "Solo Developer", [MySkills.HTML, MySkills.CSS, MySkills.JavaScript, MySkills.Monogame], "/projects/project-info", "/placeholder-photo.jpg"),
-  new ProjectInfo("Gravity Game", "Solo Developer", [MySkills.CPlusPlus, MySkills.SFML, MySkills.Box2D], "/projects/project-info", "/placeholder-photo.jpg"),
-  new ProjectInfo("Johnny Rocket", "Programmer", [MySkills.CSharp, MySkills.Monogame], "/projects/project-info", "/placeholder-photo.jpg"),
-]
-*/
-
-/*
-const featuredProjectInfo: FeaturedProjectInfo = new FeaturedProjectInfo(
-  "Eulyss",
-  "Programmer",
-  [MySkills.Unity, MySkills.CSharp],
-  "/projects/project-info",
-  "/placeholder-photo.jpg",
-  "Collaborated as part of a team of two to create an adventure game with a custom text parser that imports text files as graphs for in-game dialogue. Currently in development."
-)
-*/
-
 export default function ProjectsPage() {
 
   return (
