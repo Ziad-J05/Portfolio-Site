40f66cbb731f519f17592d18d721d0a15ca6bcb4
Ziad-J05 <143660614+Ziad-J05@users.noreply.github.com>
2025-08-15 11:29:51 -0400

    gave projects + contact pages consistent styles

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,8 +1,9 @@
 import '../index.css'
 import NavigationBar from '../components/NavigationBar'
 import MainContentArea from '../components/MainContentArea'
+import { LinkButton } from '../components/LinkButton'
 import ContentContainer from '../components/ContentContainer'
-import { SectionHeading } from '../components/Headings'
+import { SectionHeading, PageHeading } from '../components/Headings'
 import Footer from '../components/Footer'
 
 export default function ContactPage() {
@@ -10,30 +11,38 @@ export default function ContactPage() {
   return (
     <>
       <NavigationBar />
-      <MainContentArea width='4xl' content={[
-        <ContentContainer 
-          content={[
-            <SectionHeading text="Contact Me" />,
-            <div className='grid grid-cols-2 grid-rows-1 mb-4 text-lg'>
+      <PageHeading text="Contact Me" />
+        <MainContentArea width='4xl' content={[
+
+            <div className='grid grid-cols-2 grid-rows-1 w-fit mb-4 mx-auto text-xl'>
                 
-              <ul className='flex flex-col'>
-                <li>Email: <a href="mailto:ziadjaafar05@gmail.com">ziadjaafar05@gmail.com</a></li>
-                <li>Phone: <a href="tel:+17243935879">&#40;+1&#41; 724-393-5879</a></li>
+              <ul className='flex flex-col gap-6'>
+                <li>
+                  <ThingBeforeLink text="Email"/>
+                  <a href="mailto:ziadjaafar05@gmail.com">ziadjaafar05@gmail.com</a>
+                </li>
+                <li><ThingBeforeLink text="Phone"/><a href="tel:+17243935879">&#40;+1&#41; 724-393-5879</a></li>
               </ul>
 
-              <ul className='flex flex-col'>
-                <li>LinkedIn: <a href="https://www.linkedin.com/in/ziad-jaafar222/">linkedin.com/in/ziad-jaafar222</a></li>
-                <li>GitHub: <a href="https://github.com/Ziad-J05">github.com/Ziad-J05</a></li>
+              <ul className='flex flex-col gap-6'>
+                <li><ThingBeforeLink text="LinkedIn"/><a href="https://www.linkedin.com/in/ziad-jaafar222/">linkedin.com/in/ziad-jaafar222</a></li>
+                <li><ThingBeforeLink text="GitHub"/><a href="https://github.com/Ziad-J05">github.com/Ziad-J05</a></li>
+                <li><ThingBeforeLink text="Resume"/><a href="">link</a></li>
               </ul>
 
-            </div>,
+            </div>
 
-            <p className='text-lg'><a>Resume</a></p>
           ]} 
-          fixedWidth={true}
         />
-      ]} />
       <Footer />
     </>
+  )
+}
+
+function ThingBeforeLink({text}: {text: string}){
+  return(
+    <span className='mr-4 font-(family-name:--display-font) text-xl'>
+      {text}:
+    </span>
   )
 }
--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -2,7 +2,7 @@ import '../index.css'
 import NavigationBar from '../components/NavigationBar'
 import MainContentArea from '../components/MainContentArea'
 import ContentContainer from '../components/ContentContainer'
-import { SectionHeading } from '../components/Headings'
+import { SectionHeading, PageHeading } from '../components/Headings'
 import ProjectArea from '../components/ProjectArea'
 import { ProjectData } from '../components/ProjectData'
 import Footer from '../components/Footer'
@@ -14,15 +14,12 @@ export default function ProjectsPage() {
   return (
     <>
         <NavigationBar />
-        <main className='bg-[var(--secondary)] py-36 mx-auto w-6xl min-h-[calc(100vh-var(--text-lg)-8*var(--spacing))] border-l-2 border-r-2'>
-          <div className='w-5xl mx-auto mb-12'>
-            <SectionHeading text="All Projects" />
-          </div>
-            <div className="grid grid-auto-rows grid-cols-2 gap-2 px-8 w-6xl">
+        <PageHeading text="My Projects" />
+        <MainContentArea width='4xl' content={[
+          <div className="grid grid-auto-rows grid-cols-2 gap-2 px-8 w-6xl mx-auto">
               <ProjectArea projectDataList={MyProjects} />
             </div>
-          
-        </main>
+        ]} />
             
         <Footer />
     </>
