fbe8a93f48a4929cb510a87c94803e5a85f97a88
Ziad-J05 <143660614+Ziad-J05@users.noreply.github.com>
2025-08-25 15:47:21 -0400

    fixed footer responsiveness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,8 @@ import '../index.css'
 function Footer(){
   return(
     <>
-      <footer className="mx-auto text-lg w-fill py-4 bg-black text-[var(--content)] border-0">
-        <p className='w-2xl mx-auto text-center'>&copy;2025 Ziad Jaafar - Made with React and Tailwind</p>
+      <footer className="mx-auto text-lg w-full py-4 px-8 bg-black text-[var(--content)] border-0">
+        <p className='w-full mx-auto text-center'>&copy;2025 Ziad Jaafar - Made with React and Tailwind</p>
       </footer>
     </>
   )
