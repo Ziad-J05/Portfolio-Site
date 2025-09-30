6f6b7d5c6a2509c368e1638d5c8a20b469f0bf82
Ziad-J05 <143660614+Ziad-J05@users.noreply.github.com>
2025-09-30 00:34:23 -0400

    changed footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ function Footer(){
   return(
     <>
       <footer className="mx-auto text-lg w-full py-4 px-8 bg-black text-[var(--content)] border-0">
-        <p className='w-full mx-auto text-center'>&copy;2025 Ziad Jaafar - Made with React and Tailwind</p>
+        <p className='w-full mx-auto text-center'>&copy;2025 Ziad Jaafar</p>
       </footer>
     </>
   )
